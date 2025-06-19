277825f587fd7aca6b8a1c6d7b4aa6e4e703d321
Shabarish Ramaswamy <shabarishramaswamy@gmail.com>
2025-06-18 22:28:14 -0700

    Timer works but not properly

--- app/timer.tsx
+++ app/timer.tsx
@@ -1,6 +1,6 @@
 import { Ionicons } from "@expo/vector-icons";
 import { useLocalSearchParams } from 'expo-router';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function TimerScreen() {
@@ -11,9 +11,25 @@ export default function TimerScreen() {
     setIsPlaying(!isPlaying);
   }
 
-  // const handleToggle = () => {
-  //   setIsPlaying(!isPlaying); // only toggle play/pause
-  // };
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const getTime = () => {
+    const time = Date.now();
+
+    // console.log(time)
+
+    setHours(Math.floor((5 + time / (1000 * 60 * 60)) % 24));
+    setMinutes(Math.floor((time / 1000 / 60) % 60));
+    setSeconds(Math.floor((time / 1000) % 60));
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => getTime(), 1000)
+
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <View style={styles.container}>
@@ -25,7 +41,7 @@ export default function TimerScreen() {
         <TouchableOpacity onPress={handleToggle}>
           <Ionicons
             name={isPlaying ? 'pause-circle-outline' : 'play-circle-outline'}
-            size={28}
+            size={60}
             color="black"
             id="PlayPauseButton"
           />
@@ -34,7 +50,7 @@ export default function TimerScreen() {
 
 
         <Text style={styles.title}>{title}</Text>
-        <Text style={styles.timer}>1:45:12</Text>
+        <Text style={styles.timer}>{hours}:{minutes}:{seconds}</Text>
         <Text style={styles.goal}>Today's Goal: 2:00:00 +1.1%</Text>
       </View>
 
